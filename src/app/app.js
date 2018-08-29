217b33d6a70731eba0dc6b4e0a2cf6111951eeff
coomy <coomysky@gmail.com>
2018-08-29 12:59:56 +0800

    feat: add brower

--- src/app/app.js
+++ src/app/app.js
@@ -3,24 +3,10 @@
 
 import 'lodash';
 import './styles';
-import { createStore } from 'redux';
+
 import { TrackOffController } from './controllers';
 
-
-function counter(state = 0, action) {
-  switch (action.type) {
-  case 'INCREMENT':
-    return state + 1
-  case 'DECREMENT':
-    return state - 1
-  default:
-    return state
-  }
-}
-
-const store = createStore(counter);
-
-const trackeOffController = new TrackOffController(store);
+const trackeOffController = new TrackOffController();
 trackeOffController.init();
 
 
